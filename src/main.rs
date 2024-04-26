9a2c2d3e4ef5a93438e3cf748817e5f6b8f96c64
cain <gunbos1031@gmail.com>
2024-04-26 17:09:14 +0900

    refactor goal 1

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,23 @@
 use std::{env, fs};
-
-struct File {
-    path: String,
-    file_name: String,
-}
-
-impl File {
-    fn new(path: String, file_name: String) -> File {
-        File {
-            path,
-            file_name,
-        }
-    }
-}
+use std::fs::DirEntry;
 
 fn main() {
     let path = match env::current_dir() {
         Ok(path) => path,
         Err(e) => panic!("Couldn't get the current directory: {}", e),
     };
-    println!("The current directory is {}", path.display());
 
     let paths = match fs::read_dir(path) {
         Ok(paths) => paths,
         Err(e) => panic!("Couldn't read the current directory: {}", e),
     };
 
-    let files: Vec<String> = paths.map(|entry| {
-        let entry = entry.unwrap();
-         match entry.file_name().to_str() {
-            Some(file_name) => file_name.to_string(),
-            None => panic!("Couldn't convert the file name to a string"),
-        }
-    }).collect();
+    let files: Vec<DirEntry> = paths.map(|entry| entry.unwrap())
+        .collect();
 
-    for file in files {
-        println!("{}", file);
+    for file in files.iter() {
+        let file_name = file.file_name().into_string().unwrap();
+        print!("{} ", file_name);
     }
+    println!();
 }
