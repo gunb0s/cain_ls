c444ad41af0a147d9446681a396cbd6780f28b00
cain <gunbos1031@gmail.com>
2024-04-26 22:48:30 +0900

    complte goal 3, 4

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,45 @@
 use std::{env, fs};
-use std::fs::DirEntry;
+use std::fs::{DirEntry, ReadDir};
+use std::path::PathBuf;
+use colored::Colorize;
 
 fn main() {
-    let path = match env::current_dir() {
+    let path: PathBuf = match env::current_dir() {
         Ok(path) => path,
         Err(e) => panic!("Couldn't get the current directory: {}", e),
     };
 
-    let paths = match fs::read_dir(path) {
+    let paths: ReadDir = match fs::read_dir(path) {
         Ok(paths) => paths,
         Err(e) => panic!("Couldn't read the current directory: {}", e),
     };
 
-    let files: Vec<DirEntry> = paths.map(|entry| entry.unwrap())
+    let files: Vec<DirEntry> = paths
+        .map(|entry| entry.unwrap())
         .collect();
 
-    for file in files.iter() {
-        let file_name = file.file_name().into_string().unwrap();
-        print!("{} ", file_name);
-    }
+    let max_length = files
+        .iter()
+        .map(|entry| entry.file_name().len())
+        .max()
+        .unwrap();
+
+    files.iter().for_each(|entry| {
+        // let file_name = entry.file_name().to_str().unwrap();
+        let metadata = entry.metadata().unwrap();
+        let binding = entry.file_name();
+        let file_name = match binding.to_str() {
+            Some(file_name) => {
+                if metadata.is_dir() {
+                    file_name.to_string().blue()
+                } else {
+                    file_name.to_string().bold()
+                }
+            },
+            None => panic!("Couldn't convert the file name to a string"),
+        };
+        let file_name = format!("{:width$}", file_name, width = max_length);
+        print!("{file_name} ");
+    });
     println!();
 }
